1c8ac7e91169bcf1c096a8de203ae97c7159b0ff
ericbanker12 <ericbanker12@gmail.com>
2020-05-27 15:59:26 -0500

    add backend start files

---
+++ backend/src/main/java/com/mazeatk/MazeatkApplication.java
@@ -0,0 +1,13 @@
+package com.mazeatk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeatkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeatkApplication.class, args);
+	}
+
+}
