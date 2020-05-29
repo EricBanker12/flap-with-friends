4667ba1b4f3a186a713d622cc159fd07c59473f9
ericbanker12 <ericbanker12@gmail.com>
2020-05-29 18:34:52 -0500

    update front end config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MazeAtk`,
+    description: `MazeAtk is an online, multiplayer time-attack video game. Earn points by clearing each maze faster than other players.`,
+    author: `Eric Banker`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `MazeAtk`,
+        short_name: `MazeAtk`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
