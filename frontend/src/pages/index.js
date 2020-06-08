1a3470aab683c06df3787cdad61371bdd93eeea9
ericbanker12 <ericbanker12@gmail.com>
2020-06-08 18:49:15 -0500

    rewrite home page; add filler text and divs

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,13 +8,47 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div 
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '50% 50%',
+      }}
+    >
+      <section>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel tempus neque. Nunc tempor mauris ut ligula tincidunt, id accumsan risus semper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent mattis justo dui, quis sodales mi luctus vel.</p>
+        <h1>How to Play</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel tempus neque. Nunc tempor mauris ut ligula tincidunt, id accumsan risus semper.</p>
+        <div
+          style={{
+            width: 320,
+            height: 480,
+            backgroundColor: 'skyblue',
+            // margin: '0 auto',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          Gameplay Animation Here
+        </div>
+      </section>
+      <section>
+        <h1>Setup</h1>
+        <div
+          style={{
+            width: '100%',
+            height: 'calc(100% - 70px)',
+            backgroundColor: '#DDD',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          Game Setup Component
+        </div>
+      </section>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
