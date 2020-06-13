aad9203c0f88e35b31ef2e5e3d7bcce6d9971e89
ericbanker12 <ericbanker12@gmail.com>
2020-06-12 19:15:52 -0500

    add script to move built files

--- backend/src/main/java/com/flapwithfriends/FlapWithFriendsApplication.java
+++ backend/src/main/java/com/flapwithfriends/FlapWithFriendsApplication.java
@@ -1,11 +1,22 @@
 package com.flapwithfriends;
 
+import java.net.URI;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication(exclude = {
 	MongoAutoConfiguration.class,
@@ -17,6 +28,15 @@ public class FlapWithFriendsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlapWithFriendsApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator
+	public RouterFunction<ServerResponse> routerFunction() {
+		return RouterFunctions.route(
+			RequestPredicates.GET("/"),
+			req -> ServerResponse.temporaryRedirect(URI.create("/index.html")).build()
+		);
 	}
 
 }
