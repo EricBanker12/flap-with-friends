776463dcd629abe2ca410c1ea5c8ae675b683008
ericbanker12 <ericbanker12@gmail.com>
2020-07-13 20:25:29 -0500

    remove react-bootstrap

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,7 +1,6 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Navbar } from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
   <header className="navbar navbar-dark bg-primary">
