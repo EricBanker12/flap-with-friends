222987c922d9f60f45a8b40c7126335b793a03ce
ericbanker12 <ericbanker12@gmail.com>
2020-07-21 18:29:00 -0500

    add random placement and paralax scrolling to cloud sprite

--- frontend/src/components/cloudSprite.js
+++ frontend/src/components/cloudSprite.js
@@ -1,9 +1,45 @@
-import React from "react"
-import { Sprite } from "react-pixi-fiber"
+import React, { Component } from "react"
+import { Sprite, withApp } from "react-pixi-fiber"
 import { connect } from "react-redux"
 
-const CloudSprite = ({game, texture}) => {
-    return <Sprite texture={texture} />
+class CloudSprite extends Component {
+    state = {
+        x: 320,
+        y: Math.floor(Math.random() * 417),
+    }
+
+    update = (delta) => {
+        let {x, y} = this.state
+        
+        // move
+        x -= (this.props.player.dx / 2) * delta
+        
+        // new cloud
+        if (x <= -128) {
+            x = 320
+            y = Math.floor(Math.random() * 417)
+        }
+
+        this.setState({x, y})
+    }
+
+    componentDidMount() {
+        this.props.app.ticker.add(this.update)
+    }
+
+    componentWillUnmount() {
+        this.props.app.ticker.remove(this.update)
+    }
+    
+    render() {
+        return (
+            <Sprite
+                x={this.state.x  * this.props.game.scale}
+                y={this.state.y  * this.props.game.scale}
+                texture={this.props.texture}
+            />
+        )
+    }
 }
 
-export default connect(({game}) => ({game}))(CloudSprite)
+export default withApp(connect(({game, player}) => ({game, player}))(CloudSprite))
